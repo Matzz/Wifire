97d2f9806abbf55fc45bc8db609760a91a6ee0f4
Matzz <m.zakarczemny@gmail.com>
2020-11-26 22:05:50 +0100

    salt

--- app/Actions/utils.h
+++ app/Actions/utils.h
@@ -1,4 +1,11 @@
 #pragma once
 
 #include <SmingCore.h>
+#include <Crypto/Sha1.h>
+
 bool getBool(HttpRequest &request, String name);
+
+String getHash(String base) {
+		auto hash = Crypto::Sha1().calculate(base);
+		return Crypto::toString(hash);
+}
--- app/Configuration/UsersConfigProvider.h
+++ app/Configuration/UsersConfigProvider.h
@@ -2,6 +2,7 @@
 
 #include <SmingCore.h>
 #include "FileConfig.h"
+#include "../Actions/utils.h"
 
 class UserConfig {
 public:
@@ -10,6 +11,23 @@ public:
 	String salt;
 	String hash;
 	Vector<String> roles;
+
+	static UserConfig newUser(String login, String password, Vector<String> roles) {
+		auto salt = mkSalt();
+		auto cfg = UserConfig();
+		cfg.login = login;
+		cfg.enabled = true;
+		cfg.hash = getHash(salt + password);
+		cfg.salt = salt;
+		cfg.roles = roles;
+		return cfg;
+	};
+
+private:
+	static String mkSalt() {
+		auto base = String(system_get_chip_id(), 10) + String(os_get_nanoseconds(), 10);
+		return getHash(base);
+	};
 };
 
 class UsersConfig {
