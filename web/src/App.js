ac12f0f395bf93837e9d608625eb1835dd517ad3
Matzz <m.zakarczemny@gmail.com>
2021-03-06 01:04:03 +0100

    Sidebar menu

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,13 @@
 
 
-import './App.css';
+import './App.scss';
 import React from 'react';
-import { NavLink } from "react-router-dom";
 import { Route, Switch } from "react-router";
 import autoBind from 'react-autobind';
 import { withRouter } from 'react-router';
 import ApiHandler from "./ApiHandler";
 
+import { Sidebar } from "./Sidebar"
 import HomePage from "./pages/HomePage"
 import { Signin, Signout } from './pages/Auth';
 import Status from './pages/Status';
@@ -24,79 +24,50 @@ class App extends React.Component {
     super(props);
     autoBind(this);
     this.apiHandler = new ApiHandler(props.history);
-    this.apiHandler.addUpdateAuthCallback(this.updateAuth);
     this.state = {
       auth: ApiHandler.emptyAuth
     };
-  }
-  updateAuth(auth) {
-    this.setState({"auth": auth});
-  }
-  componentDidMount() {
-    this.updateAuth(this.apiHandler.getAuthJson());
-  }
-  hasRole(role) {
-    let roles = this.state.auth.roles;
-    return roles.includes(role) || roles.includes("admin");
-  }
-  isSignedin(role) {
-    var login = this.state.auth.login;
-    return login !== "" && login != null;
   }
 
   render() {
     return (
       <>
-      <nav className="main-navbar navbar navbar-expand-lg navbar-dark bg-primary">
-        <ul className="navbar-nav">
-          <li>
-            <NavLink className="navbar-brand" to="/">Wifire <span className="user_name">{this.state.auth.login}</span></NavLink>
-          </li>
-          {!this.isSignedin() && <li className="nav-item"><NavLink className="nav-link" to="/signin">Sign in</NavLink></li>}
-          {this.isSignedin() && <li className="nav-item"><NavLink className="nav-link" to="/signout">Sign out</NavLink></li>}
-          {this.hasRole("show_status") && <li className="nav-item"><NavLink className="nav-link" to="/status">Status</NavLink></li>}
-          {this.hasRole("edit_ap_config") && <li className="nav-item"><NavLink className="nav-link" to="/edit_ap">Ap config</NavLink></li>}
-          {this.hasRole("edit_station_config") && <li className="nav-item"><NavLink className="nav-link" to="/edit_station">Station config</NavLink></li>}
-          {this.hasRole("edit_gpio_config") && <li className="nav-item"><NavLink className="nav-link" to="/edit_gpio">GPIO config</NavLink></li>}
-          {this.hasRole("edit_ota_config") && <li className="nav-item"><NavLink className="nav-link" to="/edit_ota">Ota config</NavLink></li>}
-          {this.hasRole("edit_users_config") && <li className="nav-item"><NavLink className="nav-link" to="/users/list">Users</NavLink></li>}
-        </ul>
-      </nav>
+        <Sidebar apiHandler={this.apiHandler} />
       
-      <div className="container" id="content">
-        <Switch>
-          <Route exact path="/">
-            <HomePage login={this.state.auth.login} />
-          </Route>
-          <Route path="/signin">
-            <Signin apiHandler={this.apiHandler} />
-          </Route>
-          <Route path="/signout">
-            <Signout apiHandler={this.apiHandler} />
-          </Route>
-          <Route path="/status">
-            <Status apiHandler={this.apiHandler} />
-          </Route>
-          <Route path="/edit_ap">
-            <EditAp apiHandler={this.apiHandler} />
-          </Route>
-          <Route path="/edit_station">
-            <EditStation apiHandler={this.apiHandler} />
-          </Route>
-          <Route path="/edit_ota">
-            <EditOta apiHandler={this.apiHandler} />
-          </Route>
-          <Route path="/edit_gpio">
-            <EditGpio apiHandler={this.apiHandler} />
-          </Route>
-          <Route path="/users">
-            <UserManagement apiHandler={this.apiHandler} />
-          </Route>
-          <Route>
-            Not found.
-          </Route>
-        </Switch>
-      </div>
+        <div id="content">
+          <Switch>
+            <Route exact path="/">
+              <HomePage login={this.state.auth.login} />
+            </Route>
+            <Route path="/signin">
+              <Signin apiHandler={this.apiHandler} />
+            </Route>
+            <Route path="/signout">
+              <Signout apiHandler={this.apiHandler} />
+            </Route>
+            <Route path="/status">
+              <Status apiHandler={this.apiHandler} />
+            </Route>
+            <Route path="/edit_ap">
+              <EditAp apiHandler={this.apiHandler} />
+            </Route>
+            <Route path="/edit_station">
+              <EditStation apiHandler={this.apiHandler} />
+            </Route>
+            <Route path="/edit_ota">
+              <EditOta apiHandler={this.apiHandler} />
+            </Route>
+            <Route path="/edit_gpio">
+              <EditGpio apiHandler={this.apiHandler} />
+            </Route>
+            <Route path="/users">
+              <UserManagement apiHandler={this.apiHandler} />
+            </Route>
+            <Route>
+              Not found.
+            </Route>
+          </Switch>
+        </div>
       </>
     )
   }
---
+++ web/src/Sidebar.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import autoBind from 'react-autobind';
+import { withRouter } from 'react-router';
+import ApiHandler from "./ApiHandler";
+import $ from "jquery";
+
+
+function CollapsableMenu(props) {
+  const [open, setOpen] = useState(false);
+
+  let children = React.Children.toArray(props.children).filter((child) => child !== false)
+
+  var handleClick = function(event) {
+    event.preventDefault();
+    setOpen(!open)
+  };
+
+
+  return (React.Children.count(children)>0  && <>
+    <li className="nav-item">
+      <a href="/" onClick={handleClick} className={["dropdown-toggle", (open ? "active" : "")].join(" ")}>
+         {props.title}
+      </a>
+      <ul className={["list-unstyled", (open ? "open" : "closed")].join(" ")}>
+        {children}
+      </ul>
+    </li>
+  </>)
+}
+
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+    this.apiHandler = props.apiHandler;
+    this.apiHandler.addUpdateAuthCallback(this.updateAuth);
+    this.state = {
+      auth: ApiHandler.emptyAuth
+    };
+  }
+  updateAuth(auth) {
+    this.setState({"auth": auth});
+  }
+  componentDidMount() {
+    this.updateAuth(this.apiHandler.getAuthJson());
+  }
+  hasRole(role) {
+    let roles = this.state.auth.roles;
+    return roles.includes(role) || roles.includes("admin");
+  }
+  isSignedin(role) {
+    var login = this.state.auth.login;
+    return login !== "" && login != null;
+  }
+
+  handleSidebarCollapse(event) {
+    $('#sidebar').toggleClass('active');
+    $('#sidebar-collapse-btn').toggleClass('active');
+  }
+
+  render() {
+    return (
+      <>
+        <nav id="sidebar">
+          <button type="button" id="sidebar-collapse-btn" className="navbar-btn" onClick={this.handleSidebarCollapse}>
+              <span></span>
+              <span></span>
+              <span></span>
+          </button>
+          <div className="sidebar-header">
+            <NavLink className="nav-link" to="/">Wifire <span className="user_name">{this.state.auth.login}</span></NavLink>
+          </div>
+
+          <ul className="nav flex-column">
+            {!this.isSignedin() && <li className="nav-item"><NavLink className="nav-link" to="/signin">Sign in</NavLink></li>}
+            {this.isSignedin() && <li className="nav-item"><NavLink className="nav-link" to="/signout">Sign out</NavLink></li>}
+            {this.hasRole("show_status") && <li className="nav-item"><NavLink className="nav-link" to="/status">Status</NavLink></li>}
+            <CollapsableMenu title="WiFi configuration">
+                {this.hasRole("edit_ap_config") && <li className="nav-item"><NavLink className="nav-link" to="/edit_ap">Ap config</NavLink></li>}
+                {this.hasRole("edit_station_config") && <li className="nav-item"><NavLink className="nav-link" to="/edit_station">Station config</NavLink></li>}
+            </CollapsableMenu>
+            
+            {this.hasRole("edit_gpio_config") && <li className="nav-item"><NavLink className="nav-link" to="/edit_gpio">GPIO config</NavLink></li>}
+            {this.hasRole("edit_ota_config") && <li className="nav-item"><NavLink className="nav-link" to="/edit_ota">Ota config</NavLink></li>}
+            {this.hasRole("edit_users_config") && <li className="nav-item"><NavLink className="nav-link" to="/users/list">Users</NavLink></li>}
+          </ul>
+        </nav>
+      
+      </>
+    )
+  }
+}
+
+const SidebarWithRouter = withRouter(Sidebar);
+
+export {
+    SidebarWithRouter as Sidebar
+};
